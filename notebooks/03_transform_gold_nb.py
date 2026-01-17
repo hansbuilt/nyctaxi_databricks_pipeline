a0e347194bc60969bf4f40ffd7a6f4b26ad9fea2
hansbuilt <135175936+hansbuilt@users.noreply.github.com>
2026-01-17 23:40:11 +0000

    Added trip_revenue_daily_fact creation, updated declaration of spark functions

--- notebooks/03_transform_gold_nb.py
+++ notebooks/03_transform_gold_nb.py
@@ -1,12 +1,41 @@
 # Databricks notebook source
+# MAGIC %load_ext autoreload
+# MAGIC %autoreload 2
+# MAGIC # Enables autoreload; learn more at https://docs.databricks.com/en/files/workspace-modules.html#autoreload-for-python-modules
+# MAGIC # To disable autoreload; run %autoreload 0
+# MAGIC
+# MAGIC %reload_ext autoreload
+
+# COMMAND ----------
+
+# DBTITLE 1,Untitled
 import sys
 from pathlib import Path
 repo_root = str(Path().resolve().parents[0])  #relative path so we don't expose email / username
 sys.path.append(f"{repo_root}/src")
 
-from transform_gold.transform_gold_nycdata import transform_gold_consolidated_base
+from transform_gold.transform_gold_nycdata import transform_gold_consolidated_base, transform_gold_trip_revenue_daily_fact
 
 silver_base_path = "/Volumes/nyc_project/silver/"
 gold_base_path = "/Volumes/nyc_project/gold/"
+gold_consol_base_path = "/Volumes/nyc_project/gold/gold_consolidated_trips_base/"
 
 transform_gold_consolidated_base(spark, silver_base_path, gold_base_path)
+
+transform_gold_trip_revenue_daily_fact(spark, gold_consol_base_path, gold_base_path)
+
+# COMMAND ----------
+
+# DBTITLE 1,Write Parquet to Managed Table
+# read parquet
+parquet_path = "/Volumes/nyc_project/gold/gold_consolidated_trips_base"
+df = spark.read.parquet(parquet_path)
+
+# drop table first, in the event we add columns
+spark.sql("DROP TABLE IF EXISTS gold_consolidated_trips_base_fact")
+
+# write table
+df.write.mode("overwrite").saveAsTable("gold_consolidated_trips_base_fact")
+
+# display table
+display(spark.table("gold_consolidated_trips_base_fact"))
--- src/transform_gold/transform_gold_nycdata.py
+++ src/transform_gold/transform_gold_nycdata.py
@@ -1,7 +1,20 @@
-from pyspark.sql.functions import col, to_date, year, month, row_number, lit
+#from pyspark.sql.functions import col, to_date, year, month, row_number, lit
+from pyspark.sql import functions as F
 from pyspark.sql.window import Window
 
+"""
+Goals:
+- consolidated yellow and green datasets
+
+- trip revenue daily fact - trip ct, total rev, total tips, avg fare, avg tip, taxi type
+- trip demand daily fact - hour AND day, zone/location, trip ct, avg distance, avg duration, total dist, total duration, taxi type
+- trip efficiency fact - zone PU and DO pairings, taxi type, avg duration, avg distance, avg speed mph, trip count, hour/date
+- taxi type utilization - PU zone/loc, yellow trip ct, green trip ct, avg fare of each, avg duration of each
+
+""" 
+
 def transform_gold_consolidated_base(spark, silver_base_path, gold_base_path):
+
     """
     Transforms silver yellow and silver yellow into a unioned, consolidated base table.
     """
@@ -17,14 +30,14 @@ def transform_gold_consolidated_base(spark, silver_base_path, gold_base_path):
     #green only: ehail_fee, trip_type
 
     df_yellow = (df_yellow
-                .withColumn('ehail_fee', lit(0).cast('double'))
-                .withColumn('trip_type', lit(0).cast('int'))
-                .withColumn('taxi_type', lit('yellow').cast('string'))
+                .withColumn('ehail_fee', F.lit(0).cast('double'))
+                .withColumn('trip_type', F.lit(0).cast('int'))
+                .withColumn('taxi_type', F.lit('yellow').cast('string'))
     )
 
     df_green = (df_green
-                .withColumn('airport_fee', lit(0).cast('double'))
-                .withColumn('taxi_type', lit('green').cast('string'))
+                .withColumn('airport_fee', F.lit(0).cast('double'))
+                .withColumn('taxi_type', F.lit('green').cast('string'))
     )
     
     df_unioned = (
@@ -39,3 +52,48 @@ def transform_gold_consolidated_base(spark, silver_base_path, gold_base_path):
         .partitionBy('year', 'month')
         .parquet(gold_path)
     )
+
+def transform_gold_trip_revenue_daily_fact(spark, gold_consol_base_path, gold_base_path):
+
+    """
+    Transforms the gold consolidated base dataset into the trip revenue daily fact dataset.
+
+    Meant more for financial use cases.
+    """
+    #test
+    
+    gold_path = f"{gold_base_path}/gold_trip_revenue_daily_fact"
+    gold_consol_base_path = f"{gold_base_path}/gold_consolidated_trips_base"
+
+    df = spark.read.parquet(gold_consol_base_path)
+
+    df_day = (
+        df
+        .withColumn('pickup_date', F.to_date(F.col('pickup_ts')))
+        )
+
+    df_agg = (
+        df_day
+        .groupBy('pickup_date', 'taxi_type')
+        .agg(F.count("*").alias('trip_count'),
+            (F.sum('total_amount').alias('total_trip_revenue')),
+            (F.avg('total_amount').alias('avg_revenue_per_trip')),
+            (F.sum('tip_amount').alias('total_tips')),
+            (F.avg('tip_amount').alias('avg_tip_per_trip')),
+            (F.sum('trip_distance').alias('total_trip_distance')),
+            (F.avg('trip_distance').alias('avg_trip_distance')),
+            (F.sum('trip_duration_seconds').alias('total_trip_duration')),
+            (F.avg('trip_duration_seconds').alias('avg_trip_duration')),
+
+        )
+        .orderBy("pickup_date")
+    )
+
+    (
+        df_agg
+        .write
+        .mode('overwrite')
+        .partitionBy('pickup_date')
+        .parquet(gold_path)
+    )
+    

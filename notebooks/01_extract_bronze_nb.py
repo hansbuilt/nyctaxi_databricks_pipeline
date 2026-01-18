59a4631d7dfa44624135ad15c7cf0c564500ca13
hansbuilt <135175936+hansbuilt@users.noreply.github.com>
2026-01-18 02:55:32 +0000

    Added support for zone lookup table processing, reorganized notebook cells

--- notebooks/01_extract_bronze_nb.py
+++ notebooks/01_extract_bronze_nb.py
@@ -1,15 +1,26 @@
 # Databricks notebook source
-# DBTITLE 1,Cell 1
+# MAGIC %load_ext autoreload
+# MAGIC %autoreload 2
+# MAGIC # Enables autoreload; learn more at https://docs.databricks.com/en/files/workspace-modules.html#autoreload-for-python-modules
+# MAGIC # To disable autoreload; run %autoreload 0
+# MAGIC
+# MAGIC %reload_ext autoreload
+
+# COMMAND ----------
+
 import sys
 from pathlib import Path
 repo_root = str(Path().resolve().parents[0])  #relative path so we don't expose email / username
 sys.path.append(f"{repo_root}/src")
 
-from extract_bronze.extract_nyctaxi import extract_single_nyctaxi
+from extract_bronze.extract_nyctaxi import extract_single_nyctaxi, extract_zonelookup_nyctaxi
 
 raw_base_path = "/Volumes/nyc_project/raw/raw/"
 bronze_base_path = "/Volumes/nyc_project/bronze/"
 
+# COMMAND ----------
+
+# DBTITLE 1,Cell 1
 ingestion_plan = [
     {"color": "yellow", "year": 2025, "month": 1},
     {"color": "yellow", "year": 2025, "month": 2},
@@ -22,3 +33,7 @@ ingestion_plan = [
 for job in ingestion_plan:
     extract_single_nyctaxi(spark, job['color'], job['year'], job['month'], raw_base_path, bronze_base_path)
     print(f"Finished load for {job}")
+
+# COMMAND ----------
+
+extract_zonelookup_nyctaxi(spark, raw_base_path, bronze_base_path)
--- notebooks/02_transform_silver_nb.py
+++ notebooks/02_transform_silver_nb.py
@@ -1,16 +1,23 @@
 # Databricks notebook source
-# DBTITLE 1,Cell 1
 import sys
 from pathlib import Path
 repo_root = str(Path().resolve().parents[0])  #relative path so we don't expose email / username
 sys.path.append(f"{repo_root}/src")
 
-from transform_silver.transform_silver_nycdata import transform_silver_yellow_nyctaxi, transform_silver_green_nyctaxi
+from transform_silver.transform_silver_nycdata import transform_silver_yellow_nyctaxi, transform_silver_green_nyctaxi, transform_silver_zonelookup
 
 bronze_base_path = "/Volumes/nyc_project/bronze/"
 silver_base_path = "/Volumes/nyc_project/silver/"
+
+# COMMAND ----------
+
+# DBTITLE 1,Cell 1
 
 transform_silver_yellow_nyctaxi(spark, bronze_base_path, silver_base_path)
 
 transform_silver_green_nyctaxi(spark, bronze_base_path, silver_base_path)
 
+
+# COMMAND ----------
+
+transform_silver_zonelookup(spark, bronze_base_path, silver_base_path)

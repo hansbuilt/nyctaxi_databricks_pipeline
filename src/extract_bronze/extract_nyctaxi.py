7da0286595bec33db21f02e8a83300d16adb8949
hansbuilt <135175936+hansbuilt@users.noreply.github.com>
2026-01-18 02:53:55 +0000

    Added bronze extract function for zone lookup table

--- src/extract_bronze/extract_nyctaxi.py
+++ src/extract_bronze/extract_nyctaxi.py
@@ -2,7 +2,7 @@ from pyspark.sql.functions import lit, current_timestamp
 
 def extract_single_nyctaxi(spark, color, year, month, raw_base_path, bronze_base_path):
     """
-    Extracts a single month of NYC Taxi data from a raw parquet file and writes it to a bronze table.
+    Extracts a single month of NYC Taxi data from a raw parquet file and writes it to a bronze volume.
     """
 
     raw_path = f"{raw_base_path}/{color}_tripdata_{year}-{month:02d}.parquet"
@@ -22,5 +22,31 @@ def extract_single_nyctaxi(spark, color, year, month, raw_base_path, bronze_base
         .write
         .mode('append')
         .partitionBy('year', 'month')
+        .parquet(bronze_path)
+    )
+
+def extract_zonelookup_nyctaxi(spark, raw_base_path, bronze_base_path):
+    """
+    Extracts the zone lookup csv file and writes to a bronze volume.
+    """
+
+    raw_path = f"{raw_base_path}/taxi_zone_lookup.csv"
+    bronze_path = f"{bronze_base_path}/zone_lookup"
+
+    df_raw = spark.read.csv(
+        raw_path,
+        header=True,
+        inferSchema=True,
+        )
+
+    df_bronze = (
+        df_raw
+        .withColumn('ingestion_ts', current_timestamp())
+    )
+
+    (
+        df_bronze
+        .write
+        .mode('overwrite')
         .parquet(bronze_path)
     )
